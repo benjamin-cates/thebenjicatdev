659b6124d55e4598cf9eb71b398a9d014a518491
Benjamin Cates <thebenjicat@gmail.com>
2025-12-08 00:29:47 -0800

    Wrangler build

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Thebenjicat.dev
 
 Hosted here: [thebenjicat.dev](https://thebenjicat.dev)
+
+
+Build with `npm run build` and deploy with `npx wrangler deploy`
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../componenents/Header";
 
 const NotFound: React.FC = (_props: {}) => {
+    useEffect(() => {
+        const metaRobots = document.createElement('meta');
+        metaRobots.name = 'robots';
+        metaRobots.content = 'noindex';
+        document.head.appendChild(metaRobots);
+        return () => metaRobots.remove();
+    })
     return <>
         <Header></Header>
         <p>404: URL not found</p>
